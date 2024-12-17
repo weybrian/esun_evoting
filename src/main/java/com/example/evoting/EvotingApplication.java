784a1f92024e4b56483a020e6a5b85b984d4428d
wey <weybrian@gmail.com>
2024-12-17 20:36:21 +0800

    init project

---
+++ src/main/java/com/example/evoting/EvotingApplication.java
@@ -0,0 +1,13 @@
+package com.example.evoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evoting/EvotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
