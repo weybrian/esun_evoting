9a38a88efb158700cf7b94f64c93cd4d54ccaad4
wey <weybrian@gmail.com>
2025-01-12 16:34:53 +0800

    解析視圖

--- src/main/java/com/example/evoting/controller/EvotingController.java
+++ src/main/java/com/example/evoting/controller/EvotingController.java
@@ -3,38 +3,48 @@ package com.example.evoting.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.evoting.common.entity.Item;
 import com.example.evoting.service.ItemService;
 
-@RestController
+@Controller
 public class EvotingController {
 	@Autowired
 	ItemService service;
+	
+	@GetMapping("/evoting")
+	public String index() {
+		return "index";
+	}
 
 	@GetMapping("/items")
+    @ResponseBody
     public List<Item> getAllItems() {
         return service.getAllItems();//要在瀏覽器顯示 json，可 return object 或 list
     }
 
 	@PostMapping("/item")
-    public void insertItem() {
-		service.insertItem("鍵盤");
+    @ResponseBody
+    public void insertItem(@RequestBody Item item) {
+		service.insertItem(item);
     }
 
 	@PutMapping("/item/{id}")
+    @ResponseBody
     public void updateItem(@RequestBody Item item, @PathVariable Long id) {
 		service.updateItem(id, item);
     }
 	
 	@DeleteMapping("/item/{id}")
+    @ResponseBody
 	public void deleteItem(@PathVariable Long id) {
 		service.deleteItem(id);
 	}
